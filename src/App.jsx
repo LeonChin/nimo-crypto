0bf762cfaf888e7775b7a8c5b5106c08eb52b25b
Leon Chinn <leonchinn1993@gmail.com>
2025-01-05 15:57:30 +1100

    folder path bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import HomePage from "./Pages/Homepage";
+import HomePage from "./Pages/HomePage";
 import CoinPage from "./Pages/CoinPage";
 import { css } from "@emotion/css";
 import "./App.css";
